b70e1b74e1542034b9e5f4c556f44257cea6ce87
Marcos Lopes <marcoslopesg7@gmail.com>
2021-01-28 16:01:14 -0300

    Atualização

--- classes/clientes.php
+++ classes/clientes.php
@@ -28,13 +28,14 @@
 		public function getCliente($id = null)
 		{
 			$stmtEmpresas = $this->conection->prepare("
-				select c.* , v.*,
+				select c.* , v.*, es.nome as estado, es.uf, 
 				concat('http://acheiaquiali.com.br/sistema/arquivos/clientes/', c.diretorio ,'/', f.imagem) as fachada,
 				concat('http://acheiaquiali.com.br/sistema/arquivos/clientes/', c.diretorio ,'/', l.imagem) as logo, cs.nome
 				from tab_clientes  c
 				left join  tab_clientes_fachada f on c.Id = f.id_cliente 
 				left join  tab_clientes_videos v on c.Id = v.id_cliente and v.destaque = 'on'  
 				left join  tab_cidades cs on cs.Id = c.end_cidade 
+				inner join  tab_estados es on es.Id = c.end_estado 
 				left join tab_clientes_logotipo l on c.Id = l.id_cliente
 				where c.Id = :id and c.ativo = 'on' limit 1");
 
